dce29fbdb7ad8e0527290bb6c15e516826a842de
Foparty <alfonsomartingarcia@gmail.com>
2022-12-15 22:09:13 +0000

    test

--- todas/app.js
+++ todas/app.js
@@ -37,3 +37,18 @@ setInterval(() => {
 }, interval);
 
 // fin de animacion de portada
+
+// animate on scroll
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      // entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
